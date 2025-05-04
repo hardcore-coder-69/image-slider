404b0de4d64da7aeb9984c3ac6469757c66ddce7
Sunit Mishra <sunit.mishra.x@gmail.com>
2025-05-04 13:41:53 +0530

    new

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+// 1. Take image input - required
+// 2. Take animation time - optional
+// 3. Checkbox for border - optional
+// 4. Screen background color - optional
+// 5. Image gap in px - optional
+// 6. Finally Start Button
+
 const imageArray = [
-    '1.png',
-    '1 (1).png',
-    '1 (2).png',
-    '1 (3).png',
-    '1 (4).png',
-    '1 (5).png',
-    '1 (6).png',
-    '1 (7).png',
-    '1 (8).png',
     '1 (9).png',
+    '1 (8).png',
+    '1 (7).png',
+    '1 (6).png',
+    '1 (5).png',
+    '1 (4).png',
+    '1 (3).png',
+    '1 (2).png',
+    '1 (1).png',
+    '1.png',
 ];
 
 start();
@@ -28,7 +35,7 @@ async function start() {
 
 function animate() {
     const containerEl = document.getElementById('container');
-    containerEl.style.transition = 'all 80s linear';
+    containerEl.style.transition = 'all 120s linear';
     containerEl.style.transform = `translateX(-${1000 * imageArray.length}px)`;
 }
 
