2e70c01c4fbf1d82db9a4783a7c705e8b563e5de
Hardcore Coder <131332381+hardcore-coder-69@users.noreply.github.com>
2025-06-01 11:36:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -46,6 +46,10 @@
 
 
 
+
+
+const introText = "Hollywood's Biggest Losers of 2024";
+// const introText='';
 const bg = '';
 const imageArray = [
     '1.png',
@@ -67,17 +71,50 @@ const imageArray = [
     '1(16).png',
     '1(17).png',
     '1(18).png',
-    '1(19).png',
+    // '1(19).png',
 ];
-const time = '360s';
+const time = '260s';
+const animateCount = 4;
 
 
+
+
+
+
+
+const fullScreenTextContainerEl = document.getElementById('full-screen-text-container');
+const fullScreenTextEl = document.getElementById('full-screen-text');
+
+const keyboardTypingSoundEl = document.getElementById("keyboard-typing-audio");
+const audio = document.getElementById('myAudio');
+audio.load();
+keyboardTypingSoundEl.load();
+keyboardTypingSoundEl.loop = true;
 
 
 start();
 async function start() {
-    const containerEl = document.getElementById('container');
+    await sleep(2000);
+    console.log("Testing sounds...");
+    keyboardTypingSoundEl.play();
+    await sleep(2000);
+    keyboardTypingSoundEl.pause();
+    await sleep(1000);
 
+    if(introText) {
+        await showFullScreenText({
+            text: introText,
+            typingSpeed: 100,
+            textSize: '92px',
+            color: 'yellow'
+        });
+        await sleep(3000);
+        await hideFullScreenText();
+    }
+
+
+    audio.play();
+    const containerEl = document.getElementById('container');
     if(bg) {
         const bgImage = document.getElementById('bg-image');
         bgImage.setAttribute('src', `./images/${bg}`);
@@ -85,18 +122,18 @@ async function start() {
     }
     
     let imageUI = '';
-    imageArray.forEach(image => {
+    imageArray.forEach((image, index) => {
         let singleImage = `
-            <img src='./images/${image}' class='single-image shadow' />
+            <img src='./images/${image}' class='single-image shadow ${index <= (animateCount-1) ? 'image-animate-' + (index+1) : ''}' />
         `;
         imageUI += singleImage;
     })
     containerEl.innerHTML = imageUI;
-    await sleep(2000);
-    animate();
+    await sleep((animateCount * 4000) + 1000);
+    slide();
 }
 
-function animate() {
+function slide() {
     const containerEl = document.getElementById('container');
     containerEl.style.transition = `all ${time} linear`;
     containerEl.style.transform = `translateX(-${1000 * imageArray.length}px)`;
@@ -104,4 +141,71 @@ function animate() {
 
 function sleep(ms) {
     return new Promise((res, rej) => setTimeout(() => res(), ms));
+}
+
+// Show full screen text
+async function showFullScreenText({ text, typingSpeed, textSize, color }) {
+    fullScreenTextContainerEl.style.display = 'flex';
+    fullScreenTextEl.style.display = 'flex';
+
+    if (textSize) {
+        fullScreenTextEl.style.fontSize = textSize;
+    }
+
+    if(color) {
+        fullScreenTextEl.style.color = color;
+    }
+
+    await typeWriter({
+        textEl: fullScreenTextEl,
+        text: text,
+        typingSpeed: typingSpeed
+    });
+}
+
+async function hideFullScreenText() {
+    fullScreenTextContainerEl.style.transition = 'transform 0.5s linear';
+    fullScreenTextContainerEl.style.transform = 'translateX(-1400px)';
+    await sleep(500);
+    fullScreenTextEl.style.display = 'none';
+    fullScreenTextContainerEl.style.display = 'none';
+}
+
+// Text Typing Animation
+let typingText = '';
+let speed = 0;
+let charIndex = 0;
+let pauseCharacters = ['.', ','];
+async function typeWriter({ textEl, text, typingSpeed }) {
+    if (textEl && text && typingSpeed) {
+        await keyboardTypingSoundEl.play();
+        textEl.style.display = 'block';
+    };
+    if (text) typingText = text;
+    if (typingSpeed) speed = typingSpeed;
+
+    if (charIndex < typingText.length) {
+        textEl.textContent = textEl.textContent.substring(0, textEl.textContent.length - 1);
+        textEl.textContent += typingText.charAt(charIndex);
+
+        if (charIndex != typingText.length - 1) {
+            textEl.textContent += '|'
+        }
+
+        // Pause typing
+        if (pauseCharacters.includes(typingText.charAt(charIndex))) {
+            await keyboardTypingSoundEl.pause();
+            await sleep(250);
+            await keyboardTypingSoundEl.play();
+        }
+
+        charIndex++;
+        await sleep(speed);
+        await typeWriter({ textEl });
+    } else {
+        typingText = '';
+        speed = 0;
+        charIndex = 0;
+        await keyboardTypingSoundEl.pause();
+    }
 }
